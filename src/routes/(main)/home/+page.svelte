4e64e7a6bc79e89bbc8f3e8919abbee7048e5617
Lewin Kelly <heli@odex.cf>
2026-04-10 05:26:39 +0100

    Move some load functions and form actions to use remote functions

--- src/routes/(main)/home/+page.svelte
+++ src/routes/(main)/home/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { enhance } from "$app/forms"
+	import { getProjects } from "./data.remote"
 
 	const { data } = $props()
+	const projects = await getProjects()
 
 	const name = "OpenBlox"
 	const tix = 0
@@ -32,13 +34,13 @@
 	<h2>Projects</h2>
 
 	<div class="grid gap-4 grid-cols-3 pt-4 pb-8">
-		{#each data.projects as project}
+		{#each projects as project}
 			<a
 				href="project/{project.id}"
 				class="btn bg-neutral-700/50 hover:bg-neutral-800/50 active:bg-neutral-600/50 border border-neutral-500 rounded-lg">
 				<img
 					src="https://placehold.co/300x150"
-					alt="Screenshot"
+					alt="{project.name} image"
 					class="rounded-t-lg" />
 				<div class="p-4">
 					<h3 class="text-xl font-bold">{project.name}</h3>
--- src/routes/api/api.remote.ts
+++ src/routes/api/api.remote.ts
@@ -1,22 +1,20 @@
 // Contains various api methods that cannot be accessed in a page context, usually because they are requested from a component.
 
-import { error, redirect } from "@sveltejs/kit"
+import { redirect } from "@sveltejs/kit"
+import { form, getRequestEvent } from "$app/server"
 import { authorise, cookieName, invalidateSession } from "$lib/server/auth"
 
-export function load() {
-	error(403, "Nice try")
-}
-
-export const actions: import("./$types").Actions = {}
-actions.logout = async ({ cookies, locals }) => {
+export const logout = form(async () => {
+	const { cookies, locals } = getRequestEvent()
 	const { session } = await authorise(locals)
 
 	await invalidateSession(session)
 	cookies.delete(cookieName, { path: "/" })
 
 	redirect(302, "/")
-}
-actions.statusping = () => {
+})
+
+export const statusping = form(() => {
 	// does nothing
 	// hooks.server.ts will update the user's status when pinged
-}
+})

67cb97d2f3ea3a670dcb646b49f50b2fcc002fde
Lewin Kelly <heli@odex.cf>
2026-04-11 05:45:09 +0100

    Add marketplace link in sidebar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,6 +5,7 @@
 	<ul class="flex flex-col gap-2">
 		<li><a href="/home">Dashboard</a></li>
 		<li><a href="/projects">Projects</a></li>
+		<li><a href="/marketplace">Marketplace</a></li>
 		<li><a href="/voting">Voting</a></li>
 		<li><a href="/settings">Settings</a></li>
 	</ul>

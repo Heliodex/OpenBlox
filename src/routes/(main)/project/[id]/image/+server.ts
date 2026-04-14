88bafe619d1422a9115ff6c72ca84e43f4a98436
Lewin Kelly <heli@odex.cf>
2026-04-14 09:56:58 +0100

    Correctly return 404 when project image file is missing

--- src/routes/(main)/project/[id]/image/+server.ts
+++ src/routes/(main)/project/[id]/image/+server.ts
@@ -5,5 +5,8 @@ export async function GET({ params }) {
 	const { id } = params
 	if (!(await find("project", id))) error(404, "Not Found")
 
-	return new Response(Bun.file(`./data/images/${id}.avif`))
+	const file = Bun.file(`./data/images/${id}.avif`)
+	if (!(await file.exists())) error(404, "Not Found")
+
+	return new Response(file)
 }
